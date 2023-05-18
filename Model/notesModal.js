1653233b0e49c008509b43f3345891d75baf9b69
Prantosh Verma <68802039+leo1104@users.noreply.github.com>
2023-05-19 01:37:52 +0530

    first commit

---
+++ Model/notesModal.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const notesSchema = mongoose.Schema({
+    fileUrl: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: String,
+        required: true,
+
+    },
+    fileName: {
+        type: String,
+        required: true,
+    },
+    fileSize: {
+        type: String,
+        required: true,
+
+    },
+    noteTitle: {
+        type: String,
+        required: true,
+    },
+    fileType: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: [String],
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    tags: {
+        type: [String],
+        required: true,
+    },
+});
+module.exports = mongoose.model('notes', notesSchema);
